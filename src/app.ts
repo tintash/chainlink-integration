7c2683301934b1ceaabbc687b108b74a28d486b9
Nouman Ahmad <nouman.ahmad@tintash.com>
2021-06-21 15:44:01 +0500

    entity large fix

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,15 @@ import { addAsync } from '@awaitjs/express';
 import { normalizePort } from './helpers';
 import morgan from 'morgan';
 
+
+import * as bodyParser from 'body-parser';
 const app = addAsync(express());
+app.use(bodyParser.json({ type: 'application/json', limit: '500MB' }));
+
 const logger = morgan('dev');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger);
 
@@ -24,8 +28,15 @@ app.use('/cryptocompare', initiatorRouter);
 app.use( (req,res, next) => {
     const ei_ci_acckey = req.headers["x-chainlink-ea-accesskey"];
     const ei_ci_secret = req.headers["x-chainlink-ea-secret"];
+
+    console.log('Acces ', ei_ci_acckey);
+    console.log(' Secret ', ei_ci_secret);
+
+    console.log('Process env ', process.env.EI_CI_ACCESSKEY );
+    console.log('Process env secret ', process.env.EI_CI_SECRET );
+
     if(typeof ei_ci_acckey !== 'undefined' && typeof ei_ci_secret !== 'undefined') {
-        if(ei_ci_acckey === process.env.EI_CI_ACCESSKEY && ei_ci_secret === process.env.I_CI_SECRET) {
+        if(ei_ci_acckey === process.env.EI_CI_ACCESSKEY && ei_ci_secret === process.env.EI_CI_SECRET) {
             res.status(200).json({
                 status: 200,
                 message: 'Success'

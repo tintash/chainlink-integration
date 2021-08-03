031feff8de4e0613a6fd185cdb6fb941158a88a0
Nouman Ahmad <noumanahmad001@gmail.com>
2021-08-03 14:23:50 +0500

    consumer code updates

--- src/middleware/stacksConsumerMware.ts
+++ src/middleware/stacksConsumerMware.ts
@@ -1,8 +1,8 @@
 import Web3 from 'web3';
 import { readFileSync } from 'fs';
 import HDWalletProvider from '@truffle/hdwallet-provider';
-let provider = new HDWalletProvider(
-  process.env.ETH_ADDR_PRIVATE_KEY!,
+const provider = new HDWalletProvider(
+  String(process.env.ETH_ADDR_PRIVATE_KEY),
   `https://${process.env.ETH_NETWORK_NAME}.infura.io/v3/${process.env.INFURA_API_KEY}`
 );
 const web3 = new Web3(provider);
@@ -11,12 +11,12 @@ const consumerContractABI = JSON.parse(
 );
 const oracleContractABI = JSON.parse(readFileSync('src/contract-abi/oracle.json', 'utf-8'));
 
-let oracleRequestABI = oracleContractABI.find(
+const oracleRequestABI = oracleContractABI.find(
   (obj: { name: string }) => obj.name === 'OracleRequest'
 );
 const contract = new web3.eth.Contract(consumerContractABI, process.env.CONSUMER_CONTRACT_ADDR);
 
-let options = {
+const options = {
   from: process.env.ETH_ADDR,
   gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
   gas: web3.utils.toHex(150000),
@@ -28,32 +28,37 @@ export async function getRequest(oracleAddress: string, jobId: string, url: stri
       txHash: null,
       blockHash: null,
       blockNumber: null,
-      requestId: null,
-      requesterContractADDR: null,
+      requestId: '',
+      requesterContractADDR: '',
       error: { status: false, msg: null },
     };
     contract.methods
       .getRequest(oracleAddress, jobId, url, path)
       .send(options)
       .on('receipt', function (receipt: any) {
-        if (
-          receipt.status == true &&
-          receipt.transactionHash &&
-          receipt.blockHash &&
-          receipt.blockNumber &&
-          receipt.events.length != 0
-        ) {
-          let requestEventDecoded: any = web3.eth.abi.decodeLog(
+        const { status, transactionHash, blockHash, blockNumber, events } = receipt;
+        if (status == true && transactionHash && blockHash && blockNumber && events.length != 0) {
+          const requestEventDecoded: { [key: string]: string } = web3.eth.abi.decodeLog(
             oracleRequestABI.inputs,
             receipt.events[2].raw.data,
             receipt.events[2].raw.topics
           );
-          response.status = receipt.status;
-          response.txHash = receipt.transactionHash;
-          response.blockHash = receipt.blockHash;
-          response.blockNumber = receipt.blockNumber;
-          response.requestId = requestEventDecoded.requestId;
-          response.requesterContractADDR = requestEventDecoded.requester;
+          const { requestId, requester } = requestEventDecoded;
+          response = {
+            ...response,
+            status,
+            txHash: transactionHash,
+            blockHash,
+            blockNumber,
+            requestId,
+            requesterContractADDR: requester,
+          };
+          // response.status = receipt.status;
+          // response.txHash = receipt.transactionHash;
+          // response.blockHash = receipt.blockHash;
+          // response.blockNumber = receipt.blockNumber;
+          //   response.requestId = requestEventDecoded.requestId;
+          //  response.requesterContractADDR = requestEventDecoded.requester;
         }
         resolve(response);
       })

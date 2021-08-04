62a24492980aefcd4261696d9f1838fac337fc82
Sajjad <sajjad@tintash.com>
2021-08-04 22:20:49 +0500

    supported post request from clarity contract

--- src/evm/evm-initiator.ts
+++ src/evm/evm-initiator.ts
@@ -3,7 +3,7 @@ import { OracleFulfillment, parseOracleRequestValue } from '../adapter-helpers';
 import { cycleMigrations, PgDataStore } from '../datastore/postgres-store';
 import { bufferCVToASCIIString, DirectRequestParams, hexToDirectRequestParams } from '../helpers';
 import { EVMResponse, DirectRequestType } from './evm-constants';
-import { getRequest } from './evm-helper';
+import { getRequest, postRequest } from './evm-helper';
 
 export async function initiateEVMRequest(encoded_data: string) {
   try {
@@ -18,14 +18,15 @@ export async function initiateEVMRequest(encoded_data: string) {
 export async function requestChainlink(oracleTopicData: OracleFulfillment): Promise<string> {
   const jobId = bufferCVToASCIIString(oracleTopicData.spec_id);
   const paramHex = oracleTopicData.data.buffer.toString();
-  const params: DirectRequestParams = await hexToDirectRequestParams(paramHex);
+  const params: DirectRequestParams = hexToDirectRequestParams(paramHex);
   const urlPath = params.get !== undefined ? params.get : params.post;
+  const body = params.post != undefined ? params.body : '';
   if (urlPath === undefined) {
     throw new Error('invalid direct request parameters.');
   }
   const requestType: DirectRequestType =
     params.get !== undefined ? DirectRequestType.GET : DirectRequestType.POST;
-  const evmResponse = await makeEVMContractCall(jobId, requestType, urlPath, params.path);
+  const evmResponse = await makeEVMContractCall(jobId, requestType, urlPath, params.path, body);
   const db: PgDataStore = await PgDataStore.connect();
   const client: PoolClient = await db.pool.connect();
   await db.updateOracleRequest(oracleTopicData, evmResponse.requestId);
@@ -36,7 +37,8 @@ export async function makeEVMContractCall(
   jobId: string,
   requestType: DirectRequestType,
   url: string,
-  decodePath: string
+  decodePath: string,
+  body: string
 ): Promise<EVMResponse> {
   const oracleAddr = String(process.env.ETHEREUM_CHAINLINK_ORACLE_CONTRACT);
   let result: EVMResponse;
@@ -44,7 +46,7 @@ export async function makeEVMContractCall(
     case DirectRequestType.POST:
       result = await getRequest(oracleAddr, jobId, url, decodePath);
     case DirectRequestType.GET:
-      result = await getRequest(oracleAddr, jobId, url, decodePath);
+      result = await postRequest(oracleAddr, jobId, url, decodePath, body);
   }
   return result;
 }

0a0d721ec44fe32ce202e738fd35f47be4dde679
Zeeshan Akram <zeeshan.akram@tintash.com>
2021-09-04 00:38:29 +0500

    fix: bridge creation issue resolved

--- src/configure-chainlink.ts
+++ src/configure-chainlink.ts
@@ -95,132 +95,149 @@ export interface Attributes {
 
 //----------------------------------------------------------------
 async function hasExternalInitiator(name: string, cookie: string): Promise<boolean> {
-  const EIs = await chainlinkfetch<ExternalInitiators>(
-    'http://localhost:6688/v2/external_initiators',
-    'GET',
-    cookie,
-    undefined
-  );
-  return EIs.data.some(initiator => initiator.attributes.name === name);
+  try {
+    const EIs = await chainlinkfetch<ExternalInitiators>(
+      'http://localhost:6688/v2/external_initiators',
+      'GET',
+      cookie,
+      undefined
+    );
+    return EIs.data.some(initiator => initiator.attributes.name === name);
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 async function hasBridge(name: string, cookie: string): Promise<boolean> {
-  const bridge = await chainlinkfetch<Bridge>(
-    `http://localhost:6688/v2/bridge_types/${name}`,
-    'GET',
-    cookie,
-    undefined
-  );
-  return bridge.data.attributes.name === name;
+  try {
+    const bridge = await chainlinkfetch<Bridge>(
+      `http://localhost:6688/v2/bridge_types/${name}`,
+      'GET',
+      cookie,
+      undefined
+    );
+    return bridge.data.attributes.name === name;
+  } catch (error) {
+    console.log('error00: ', error.code);
+    if (error.code === 404) return false;
+    throw new Error(error);
+  }
 }
 
 export async function createExternalInitiator(cookie: string): Promise<string> {
-  const eiName = String(process.env.CHAINLINK_EI_NAME);
-  const body = JSON.stringify({
-    name: eiName,
-    url: String(process.env.CHAINLINK_EI_URL),
-  });
+  try {
+    const eiName = String(process.env.CHAINLINK_EI_NAME);
+    const body = JSON.stringify({
+      name: eiName,
+      url: String(process.env.CHAINLINK_EI_URL),
+    });
 
-  if (await hasExternalInitiator(eiName, cookie)) {
-    console.log(`External initiator with name ${eiName} already exists`);
-    return eiName;
-  } else {
-    const ei = await chainlinkfetch<NewExternalInitiator>(
-      'http://localhost:6688/v2/external_initiators',
-      'POST',
-      cookie,
-      body
-    );
+    if (await hasExternalInitiator(eiName, cookie)) {
+      console.log(`External initiator with name ${eiName} already exists`);
+      return eiName;
+    } else {
+      const ei = await chainlinkfetch<NewExternalInitiator>(
+        'http://localhost:6688/v2/external_initiators',
+        'POST',
+        cookie,
+        body
+      );
 
-    console.log(`created new external initiator: "${ei.data.attributes.name}"`);
-    process.env['EI_IC_ACCESSKEY'] = ei.data.attributes.incomingAccessKey;
-    process.env['EI_IC_SECRET'] = ei.data.attributes.incomingSecret;
-    process.env['EI_CI_ACCESSKEY'] = ei.data.attributes.outgoingToken;
-    process.env['EI_CI_SECRET'] = ei.data.attributes.outgoingSecret;
-    setEnvValue('EI_IC_ACCESSKEY', ei.data.attributes.incomingAccessKey);
-    setEnvValue('EI_IC_SECRET', ei.data.attributes.incomingSecret);
-    setEnvValue('EI_CI_ACCESSKEY', ei.data.attributes.outgoingToken);
-    setEnvValue('EI_CI_SECRET', ei.data.attributes.outgoingSecret);
-    console.log(`updated external initiator environment vars`);
-    return ei.data.attributes.name;
+      console.log(`created new external initiator: "${ei.data.attributes.name}"`);
+      process.env['EI_IC_ACCESSKEY'] = ei.data.attributes.incomingAccessKey;
+      process.env['EI_IC_SECRET'] = ei.data.attributes.incomingSecret;
+      process.env['EI_CI_ACCESSKEY'] = ei.data.attributes.outgoingToken;
+      process.env['EI_CI_SECRET'] = ei.data.attributes.outgoingSecret;
+      setEnvValue('EI_IC_ACCESSKEY', ei.data.attributes.incomingAccessKey);
+      setEnvValue('EI_IC_SECRET', ei.data.attributes.incomingSecret);
+      setEnvValue('EI_CI_ACCESSKEY', ei.data.attributes.outgoingToken);
+      setEnvValue('EI_CI_SECRET', ei.data.attributes.outgoingSecret);
+      console.log(`updated external initiator environment vars`);
+      return ei.data.attributes.name;
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 }
 
 export async function createBridge(cookie: string): Promise<string> {
-  const bridgeName = String(process.env.CHAINLINK_BRIDGE_NAME);
-  const body = JSON.stringify({
-    name: bridgeName,
-    url: String(process.env.CHAINLINK_BRIDGE_URL),
-  });
+  try {
+    const bridgeName = String(process.env.CHAINLINK_BRIDGE_NAME);
+    const body = JSON.stringify({
+      name: bridgeName,
+      url: String(process.env.CHAINLINK_BRIDGE_URL),
+    });
 
-  if (await hasBridge(bridgeName, cookie)) {
-    console.log(`Bridge with name ${bridgeName} already exists`);
-    return bridgeName;
-  } else {
-    const bridge = await chainlinkfetch<NewBridge>(
-      'http://localhost:6688/v2/bridge_types',
-      'POST',
-      cookie,
-      body
-    );
+    if (await hasBridge(bridgeName, cookie)) {
+      console.log(`Bridge with name ${bridgeName} already exists`);
+      return bridgeName;
+    } else {
+      const bridge = await chainlinkfetch<NewBridge>(
+        'http://localhost:6688/v2/bridge_types',
+        'POST',
+        cookie,
+        body
+      );
 
-    console.log(`created new bridge: "${bridge.data.attributes.name}"`);
-    return bridge.data.attributes.name;
+      console.log(`created new bridge: "${bridge.data.attributes.name}"`);
+      return bridge.data.attributes.name;
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 }
 
 export async function createJobs(cookie: string): Promise<void> {
-  if (String(process.env.CREATE_SAMPLE_JOBS) === 'true') {
-    const getJob = createJob(JSON.stringify(getRequestJob), cookie, 'CHAINLINK_GET_JOB_ID');
-    const postJob = createJob(JSON.stringify(postRequestJob), cookie, 'CHAINLINK_POST_JOB_ID');
-    Promise.all([getJob, postJob]).then(([getJobId, postJobId]) => {
-      if (getJobId !== '' && postJobId !== '') {
-        setEnvValue('CREATE_SAMPLE_JOBS', 'false');
-      }
+  if (String(process.env.CREATE_SAMPLE_JOBS) === 'true')
+    Promise.all([
+      createJob(JSON.stringify(getRequestJob), cookie, 'CHAINLINK_GET_JOB_ID'),
+      createJob(JSON.stringify(postRequestJob), cookie, 'CHAINLINK_POST_JOB_ID'),
+    ]).then(([getJobId, postJobId]) => {
+      if (getJobId !== '' && postJobId !== '') setEnvValue('CREATE_SAMPLE_JOBS', 'false');
     });
-  } else {
-    console.log(`Skipping sample jobs creation. "CREATE_SAMPLE_JOBS" is set to "false"`);
-  }
+  else console.log(`Skipping sample jobs creation. "CREATE_SAMPLE_JOBS" is set to "false"`);
 }
 
 async function createJob(jobPayload: string, cookie: string, envVar: string): Promise<string> {
-  const job = await chainlinkfetch<NewJob>(
-    'http://localhost:6688/v2/specs',
-    'POST',
-    cookie,
-    jobPayload
-  );
-  console.log(`created new job with ID: ${job.data.id}`);
-  process.env[envVar] = job.data.id;
-  setEnvValue(envVar, job.data.id);
-  return job.data.id;
+  try {
+    const job = await chainlinkfetch<NewJob>(
+      'http://localhost:6688/v2/specs',
+      'POST',
+      cookie,
+      jobPayload
+    );
+    console.log(`created new job with ID: ${job.data.id}`);
+    process.env[envVar] = job.data.id;
+    setEnvValue(envVar, job.data.id);
+    return job.data.id;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-function chainlinkfetch<T>(
+async function chainlinkfetch<T>(
   url: string,
   method: string,
   cookie: string,
   body: string | undefined
 ): Promise<T> {
-  return fetch(url, {
-    method: method,
-    headers: {
-      'Content-Type': 'application/json',
-      cookie,
-    },
-    body: body,
-  })
-    .then(response => {
-      if (!response.ok) {
-        response.json().then(response => {
-          throw new Error(`{ error: ${JSON.stringify(response)}, code: ${response.status} }`);
-        });
-      }
-      return response.json().then(response => response as T);
-    })
-    .catch(error => {
-      throw new Error(error);
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        cookie,
+      },
+      body: body,
     });
+    if (!response.ok) {
+      const jsonRes = await response.clone().json();
+      throw { error: { jsonRes }, code: response.status };
+    }
+    return response.json().then(response => response as T);
+  } catch (error) {
+    throw error;
+  }
 }
 
 function setEnvValue(key: string, value: string) {

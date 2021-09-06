500dcbd79c84e6ae1ffacd0b18ff8a848e21e473
Hassan_21 <hassan.ahmed@tintash.com>
2021-09-07 03:38:49 +0500

    added test cases for getJobSpecMinPayment

--- src/tests/initiator-helpers-tests.ts
+++ src/tests/initiator-helpers-tests.ts
@@ -6,25 +6,26 @@ import {
 import { mockFunction } from '../jestHelpers';
 jest.mock('node-fetch');
 import fetch from 'node-fetch';
-const { Response } = jest.requireActual('node-fetch');
+// import { Headers } from 'node-fetch';
+const { Response, Headers } = jest.requireActual('node-fetch');
 const mockfetch = mockFunction(fetch);
 
 describe('Validate payment and jobcost comparison', () => {
-  test('Returns True on payment > Jobcost', () => {
+  test('Returns True on payment > jobcost', () => {
     const payment = BigInt(20);
     const jobcost = BigInt(10);
     const result = validatePayment(payment, jobcost);
     expect(result).resolves.toBe(true);
   });
 
-  test('Returns True on payment = Jobcost', () => {
+  test('Returns true on payment = jobcost', () => {
     const payment = BigInt(10);
     const jobcost = BigInt(10);
     const result = validatePayment(payment, jobcost);
     expect(result).resolves.toBe(true);
   });
 
-  test('Returns False on payment < jobcost', () => {
+  test('Returns false on payment < jobcost', () => {
     const payment = BigInt(10);
     const jobcost = BigInt(20);
     const result = validatePayment(payment, jobcost);
@@ -32,50 +33,145 @@ describe('Validate payment and jobcost comparison', () => {
   });
 });
 
-describe('Tests implementation for fetching Session Cookie', () => {
+describe('Tests implementation for fetching session cookie', () => {
   beforeEach(() => {
     mockfetch.mockReset();
   });
-    test('Fetches cookie with valid credentials', async () => {
-      mockfetch.mockResolvedValueOnce(
-        new Response(
-          'explorer=%7B%22status%22%3A%22disconnected%22%2C%22url%22%3A%22%22%7D; Path=/; SameSite=Strict; clsession=MTYzMDg3MDMxN3xEdi1CQkFFQ180SUFBUkFCRUFBQVJ2LUNBQUVHYzNSeWFXNW5EQTRBREdOc2MyVnpjMmx2Ymw5cFpBWnpkSEpwYm1jTUlnQWdZVFZqTXpjM1kyTTJOamRqTkRJNU9UZzJabVV3TURWaVl6Smlaamt5WWpNPXxtVt1qY7sE0BIbuS8rX6x9eDjdY1VjIYfSR1bSb7Hv9A==; Expires=Tue, 05 Oct 2021 19:31:57 GMT; Max-Age=2592000; HttpOnly'
-        )
-      );
-      
-      expect.assertions(3);
-      await expect(getChainlinkClientSessionCookie()).resolves.toBe('null');
-      expect(mockfetch).toHaveBeenCalledTimes(1);
-      expect(mockfetch).toHaveBeenCalledWith('http://localhost:6688/sessions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: 'test@tintash.com',
-          password: '12345678',
-        }),
-      });
-    });
+  test('Fetches cookie with valid credentials', async () => {
+    const response = new Response();
+    response.headers.set(
+      'set-cookie',
+      'explorer=%7B%22status%22%3A%22disconnected%22%2C%22url%22%3A%22%22%7D; Path=/; SameSite=Strict, clsession=MTYzMDg3MDMxN3xEdi1CQkFFQ180SUFBUkFCRUFBQVJ2LUNBQUVHYzNSeWFXNW5EQTRBREdOc2MyVnpjMmx2Ymw5cFpBWnpkSEpwYm1jTUlnQWdZVFZqTXpjM1kyTTJOamRqTkRJNU9UZzJabVV3TURWaVl6Smlaamt5WWpNPXxtVt1qY7sE0BIbuS8rX6x9eDjdY1VjIYfSR1bSb7Hv9A==; Expires=Tue, 05 Oct 2021 19:31:57 GMT; Max-Age=2592000; HttpOnly'
+    );
+    mockfetch.mockResolvedValueOnce(response);
 
-    test('Thows Exception', async () => {
-      mockfetch.mockRejectedValueOnce(new Error('Error Fetching Cookie'));
-      expect.assertions(3);
-      try {
-        await getChainlinkClientSessionCookie();
-      } catch (err) {
-        expect(err).toEqual(new Error('Error Fetching Cookie'));
-      }
-      expect(mockfetch).toHaveBeenCalledTimes(1);
-      expect(mockfetch).toHaveBeenCalledWith('http://localhost:6688/sessions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: 'test@tintash.com',
-          password: '12345678',
-        }),
-      });
+    expect.assertions(3);
+    await expect(getChainlinkClientSessionCookie()).resolves.toBe(
+      'explorer=%7B%22status%22%3A%22disconnected%22%2C%22url%22%3A%22%22%7D; Path=/; SameSite=Strict; clsession=MTYzMDg3MDMxN3xEdi1CQkFFQ180SUFBUkFCRUFBQVJ2LUNBQUVHYzNSeWFXNW5EQTRBREdOc2MyVnpjMmx2Ymw5cFpBWnpkSEpwYm1jTUlnQWdZVFZqTXpjM1kyTTJOamRqTkRJNU9UZzJabVV3TURWaVl6Smlaamt5WWpNPXxtVt1qY7sE0BIbuS8rX6x9eDjdY1VjIYfSR1bSb7Hv9A==; Expires=Tue, 05 Oct 2021 19:31:57 GMT; Max-Age=2592000; HttpOnly'
+    );
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith('http://localhost:6688/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: 'test@tintash.com',
+        password: '12345678',
+      }),
     });
+  });
+
+  test('Thows Exception', async () => {
+    mockfetch.mockRejectedValueOnce(new Error('Error Fetching Cookie'));
+    expect.assertions(3);
+    try {
+      await getChainlinkClientSessionCookie();
+    } catch (err) {
+      expect(err).toEqual(new Error('Error Fetching Cookie'));
+    }
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith('http://localhost:6688/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: 'test@tintash.com',
+        password: '12345678',
+      }),
+    });
+  });
+});
+
+describe('Tests for min job spec payment', () => {
+  beforeEach(() => {
+    mockfetch.mockReset();
+  });
+
+  test('Fetches min job payment against jobID', async () => {
+    mockfetch.mockResolvedValueOnce(
+      new Response(JSON.stringify({ data: { id: 1, attributes: { minPayment: 2 } } }))
+    );
+    const jobID = 'ebb3bff75a1846fe93bf2f12c73ca2cb';
+    const cookie = 'validTestCookie';
+    expect.assertions(4);
+
+    const result = await getJobSpecMinPayment(jobID, cookie);
+    expect(result).toBe(2);
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith(`http://localhost:6688/v2/specs/${jobID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie: cookie,
+      },
+    });
+    expect(typeof result).toBe('number');
+  });
+
+  test('returns default min job payment = 1 when payment is not specified in jobspec', async () => {
+    mockfetch.mockResolvedValueOnce(
+      new Response(
+        JSON.stringify({ data: { id: 'ebb3bff75a1846fe93bf2f12c73ca2cb', attributes: {} } })
+      )
+    );
+    const jobID = 'ebb3bff75a1846fe93bf2f12c73ca2cb';
+    const cookie = 'validTestCookie';
+    // expect.assertions(4);
+
+    const result = await getJobSpecMinPayment(jobID, cookie);
+    expect(result).toBe(1);
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith(`http://localhost:6688/v2/specs/${jobID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie: cookie,
+      },
+    });
+    expect(typeof result).toBe('number');
+  });
+
+  test('Thows exception on invalid jobID or cookie or both', async () => {
+    const jobID = 'ebb3bff75a1846fe93bf2f12c73ca2cb';
+    const cookie = 'InvalidTestCookie';
+
+    mockfetch.mockResolvedValueOnce(new Response(JSON.stringify({})));
+    expect.assertions(3);
+    try {
+      await getJobSpecMinPayment(jobID, cookie);
+    } catch (err) {
+      expect(err).toEqual(new Error('Invalid Job ID or Cookie While Fetching JobSpec MinPayment'));
+    }
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith(`http://localhost:6688/v2/specs/${jobID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie: cookie,
+      },
+    });
+  });
+
+  test('Thows exception on unknown failure', async () => {
+    const jobID = 'ebb3bff75a1846fe93bf2f12c73ca2cb';
+    const cookie = 'InvalidTestCookie';
+
+    mockfetch.mockRejectedValueOnce(new Error('{ error: Failed Fetching JobSpec MinPayment }'));
+    // expect.assertions(3);
+    try {
+      await getJobSpecMinPayment(jobID, cookie);
+    } catch (err) {
+      expect(err).toEqual(new Error('{ error: Failed Fetching JobSpec MinPayment }'));
+    }
+    expect(mockfetch).toHaveBeenCalledTimes(1);
+    expect(mockfetch).toHaveBeenCalledWith(`http://localhost:6688/v2/specs/${jobID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie: cookie,
+      },
+    });
+  });
 });
